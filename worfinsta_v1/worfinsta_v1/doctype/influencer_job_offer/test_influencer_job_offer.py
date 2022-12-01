bbe88b985d8a35a2255ecf62a05b4e09fcfe13c8
1byZero <amitkp66@live.com>
2022-12-01 15:33:45 +0530

    worfinsta_v1.01

---
+++ worfinsta_v1/worfinsta_v1/doctype/influencer_job_offer/test_influencer_job_offer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Amit kumar @ Worf and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testinfluencerjoboffer(FrappeTestCase):
+	pass
