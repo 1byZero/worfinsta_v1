42bc0bebb652969c357173cfe708c4a20cdaef5b
1byZero <amitkp66@live.com>
2022-12-22 04:02:18 +0000

    initial stage

---
+++ worfinsta_v1/worfinsta_v1/doctype/influencer_profiles/influencer_profiles.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Amit kumar @ Worf and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class InfluencerProfiles(Document):
+	pass
--- worfinsta_v1/worfinsta_v1/doctype/influencer_profiles/test_influencer_profiles.py
+++ worfinsta_v1/worfinsta_v1/doctype/influencer_profiles/test_influencer_profiles.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestInfluencer(FrappeTestCase):
+class TestInfluencerProfiles(FrappeTestCase):
 	pass
